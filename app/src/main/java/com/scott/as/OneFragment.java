81152b06a8999ccbb0a607aa496b5cecd050aa39
yuanjian <yuanjianjava@163.com>
2016-09-02 13:30:52 +0800

    在fragment中添加一个方法

--- app/src/main/java/com/scott/as/OneFragment.java
+++ app/src/main/java/com/scott/as/OneFragment.java
@@ -2,7 +2,11 @@ package com.scott.as;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Function:
@@ -18,6 +22,13 @@ public class OneFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
 
     }
 }
