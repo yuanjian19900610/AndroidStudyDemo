67fc8c82474c792ff36f2837d1b17b681a870e34
yuanjian <yuanjianjava@163.com>
2016-09-02 14:09:00 +0800

    添加日志

--- app/src/main/java/com/scott/as/OneFragment.java
+++ app/src/main/java/com/scott/as/OneFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -34,7 +35,10 @@ public class OneFragment extends Fragment {
             rootView=inflater.inflate(R.layout.fragment_one,container,false);
         }
 
+        Log.i("smarhit","test");
         return rootView;
 
     }
+
+
 }
